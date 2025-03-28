ab8d04fbb7baf1b89118572317326810b6e69e26
AluMati <AluMati@PC17>
2025-03-28 14:08:35 +0100

    implentacion de resta y multiplicacion

--- entornos/src/entornos/d/calculadora.java
+++ entornos/src/entornos/d/calculadora.java
@@ -4,7 +4,12 @@ public class calculadora {
 	public int sumar(int a, int b) {
 		return a + b;
 	}
-	
+	public int restar(int a, int b) {
+		return a - b;
+	}
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
 	public static void main(String[] args) {
 		calculadora calc = new calculadora();
 		System.out.println("Resultado de la suma: " + calc.sumar(5, 3));
